23790625dd2125117b3d0b8dbfee1121791394b1
wmt36 <wmt36@Williams-iMac.local>
2020-07-31 21:45:23 -0600

    updated the javascript and used jquery to help show funtions

--- script.js
+++ script.js
@@ -1,65 +1,68 @@
-const characterAmountRange = document.getElementById("characterAmountRange")
-const characterAmountNumber = document.getElementById("characterAmountNumber")
-const includeUppercaseElement = document.getElementById("includeUppercase")
-const includeLowercaseElement = document.getElementById("includeLowercase")
-const includeSymbolsElement = document.getElementById("includeSymbols")
-const includeNumbersElement = document.getElementById("includeNumbers")
-
-const form = document.getElementById("passwordGenratorForm")
-const passwordDisplay = document.getElementById("passwordDisplay")
-
-const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
-    arrayFromLowtoHigh(58, 64)
-).concat(
-    arrayFromLowtoHigh(91, 96)
-).concat(
-    arrayFromLowtoHigh(123, 126)
-)
+$(document).ready(function () {
 
 
-characterAmountNumber.addEventListener("input", syncCharacterAmount)
-characterAmountRange.addEventListener("input", syncCharacterAmount)
+    const characterAmountNumber = document.getElementById("characterAmountNumber")
+    const includeUppercaseElement = document.getElementById("includeUppercase")
+    const includeLowercaseElement = document.getElementById("includeLowercase")
+    const includeSymbolsElement = document.getElementById("includeSymbols")
+    const includeNumbersElement = document.getElementById("includeNumbers")
 
-form.addEventListener("submit", e => {
-    e.preventDefault()
-    const characterAmount = characterAmountNumber.value
-    const includeUppercase = includeUppercaseElement.checked
-    const includeLowercase = includeLowercaseElement.checked
-    const includeSymbols = includeSymbolsElement.checked
-    const includeNumbers = includeNumbersElement.checked
+  
+    
 
-    const password = generatePassword(characterAmount, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
-    passwordDisplay.innerText = password
-})
-function generatePassword(characterAmount, includeUppercase, includeSymbols, includeNumbers, includeLowercase) {
-    let charCodes = LOWERCASE_CHAR_CODES
-    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-    if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES)
+    const UPPERCASE_CHAR_CODES = charCodeArray(65, 90)
+    const LOWERCASE_CHAR_CODES = charCodeArray(97, 122)
+    const NUMBER_CHAR_CODES = charCodeArray(48, 57)
+    const SYMBOL_CHAR_CODES = charCodeArray(33, 47).concat( charCodeArray(58, 64)
+    ).concat( charCodeArray(91, 96)
+    ).concat( charCodeArray(123, 126)
+    )
 
-    const passwordCharacters = []
-    for (let i = 0; i < characterAmount; i++) {
-        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-        passwordCharacters.push(String.fromCharCode(characterCode))
+
+    characterAmountNumber.addEventListener("input", syncCharacterAmount)
+
+
+    $(".form").on("submit", e => {
+        e.preventDefault()
+        var characterAmount = characterAmountNumber.value
+        var includeUppercase = includeUppercaseElement.checked
+        var includeLowercase = includeLowercaseElement.checked
+       var includeSymbols = includeSymbolsElement.checked
+       var includeNumbers = includeNumbersElement.checked
+
+        const password = generatePassword(characterAmount, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
+        $("#passwordDisplay").text(password);
+    })
+    
+
+    function generatePassword(characterAmount, includeUppercase, includeSymbols, includeNumbers, includeLowercase) {
+        let charCodes = LOWERCASE_CHAR_CODES
+        if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+        if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+        if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+        if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES)
+
+        const passwordCharacters = []
+        for (let i = 0; i < characterAmount; i++) {
+            const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+            passwordCharacters.push(String.fromCharCode(characterCode))
+        }
+        return passwordCharacters.join(" ")
     }
-    return passwordCharacters.join(" ")
-}
 
-function arrayFromLowtoHigh(low, high) {
-    const array = []
-   for (let i = low; i <= high; i++) {
-    array.push(i)
+    function charCodeArray(low, high) {
+        const array = []
+        for (let i = low; i <= high; i++) {
+            array.push(i)
+        }
+        return array
     }
-return array
-}
 
 
-function syncCharacterAmount(e) {
-    const value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value 
-}
+    function syncCharacterAmount(e) {
+        const value = e.target.value
+        characterAmountNumber.value = value
+    }
+
+});
+
